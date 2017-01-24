feea7f1a2f8de9d572240e3f3f49677092ed5536
Sare Samadou <saresamadou1991@gmail.com>
2017-01-24 19:35:47 +0100

    First commit

---
+++ src/main/java/aboudou/samadou/batch/reservation_app_batch/App.java
@@ -0,0 +1,13 @@
+package aboudou.samadou.batch.reservation_app_batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
